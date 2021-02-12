dd26b59cf2f271ab09f25217110dc987ce2ae3ac
Alexander Redding <alexredd99@gmail.com>
2021-02-12 13:11:49 -0600

    Added new updates

--- software/apps/ble_ess_example/main.c
+++ software/apps/ble_ess_example/main.c
@@ -28,6 +28,21 @@ static simple_ble_service_t environmental_sensing_service = {{
               0x00,0x10,0x00,0x00,0x1A,0x18,0x00,0x00}
 }};
 
+static simple_ble_char_t Elevation = {.uuid16 = 0x2A6C}; // temperature handle
+static simple_ble_char_t Pressure = {.uuid16 = 0x2A6D};
+static simple_ble_char_t Temperature = {.uuid16 = 0x2A6E};
+static simple_ble_char_t Humidity = {.uuid16 = 0x2A6F};
+static simple_ble_char_t Pollen_Concentration = {.uuid16 = 0x2A75};
+
+static int32_t elevation_int = 185 & 0xFFFFFF;  // sint24
+static uint32_t pressure_int = 99000;    // change place?
+static int16_t temperature_int = -1722; // change place?
+static uint16_t humidity_int = 5300; // change place? 
+static uint32_t pollen_int =  343 & 0xFFFFFF; // Change place?
+
+
+
+
 /*******************************************************************************
  *   State for this application
  ******************************************************************************/
@@ -45,7 +60,27 @@ int main(void) {
   // Setup BLE
   simple_ble_app = simple_ble_init(&ble_config);
 
-  simple_ble_add_service(&environmental_sensing_service);
+  simple_ble_add_service(&environmental_sensing_service); // Init service
+  
+  simple_ble_add_characteristic(1, 0, 1, 0,   // Elevation handle
+      sizeof(elevation_int), (uint8_t*)&elevation_int,  // change sizeof?
+      &environmental_sensing_service, &Elevation);
+  
+   simple_ble_add_characteristic(1, 0, 1, 0,   // Pressure handle
+      sizeof(pressure_int), (uint8_t*)&pressure_int,  // change sizeof?
+      &environmental_sensing_service, &Pressure);
+
+    simple_ble_add_characteristic(1, 0, 1, 0,   // Temperature handle
+      sizeof(temperature_int), (uint8_t*)&temperature_int,  // change sizeof?
+      &environmental_sensing_service, &Temperature);
+
+    simple_ble_add_characteristic(1, 0, 1, 0,   // Humidity handle
+      sizeof(humidity_int), (uint8_t*)&humidity_int,  // change sizeof?
+      &environmental_sensing_service, &Humidity);
+       
+    simple_ble_add_characteristic(1, 0, 1, 0,   // Pollen handle
+      sizeof(pollen_int), (uint8_t*)&pollen_int,  // change sizeof?
+      &environmental_sensing_service, &Pollen_Concentration);
 
   // Start Advertising
   simple_ble_adv_only_name();
